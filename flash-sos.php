c68faf439a16e1111820a44d4573636628084b10
jcmurty <james@murty.org>
2014-01-24 15:57:54 +1100

    making SOS a bit more generic
    still needs a lot of optimisation

--- flash-sos.php
+++ flash-sos.php
@@ -8,6 +8,15 @@
  * @version 1.0
  * @copyright 2013 Avi Miller
  * @license MIT
+ 
+ * from http://en.wikipedia.org/wiki/Morse_code
+ * International Morse code is composed of five elements:
+ * short mark, dot or "dit" (·) — "dot duration" is one time unit long
+ * longer mark, dash or "dah" (–) — three time units long
+ * inter-element gap between the dots and dashes within a character — one dot duration or one unit long
+ * short gap (between letters) — three time units long
+ * medium gap (between words) — seven time units long[1]
+ *
  */
  
 include 'class.holiday.secretapi.php';
@@ -17,33 +26,47 @@ $longdelay = 3000000;
 
 // Need to pass the IP address or hostname of the Holiday on the command-line
 
+$message = (".",".",".","|","-","-","-","|",".",".",".");
+
+$unit=100000;
+
 $holiday = new HolidaySecretAPI($argv[1]);  
 $holiday->fill(0,0,0);
 $holiday->render();
 
 $r = 240; $g = 0; $b = 0;
 
-$state = 1;
-
 while (true) {
-
-// fast flash
-
-	for ( $i = 0 ; $i < 3 ; $i++) {
-		$holiday->fill($r, $g, $b);
-		$holiday->render();
-		if ( $state ) {
-				echo "blip";
-				usleep($shortdelay);
+	for ( $i = 0 ; $i < count($message) ; $i++) {
+		switch ($message[$i]) {
+			case ".":
+				echo ".";
+				$holiday->fill($r, $g, $b);
+				$holiday->render();
+				usleep($unit);
+				$holiday->fill(0,0,0);
+				$holiday->render();
+				break;
+			case "-":
+				echo "-";
+				$holiday->fill($r, $g, $b);
+				$holiday->render();
+				usleep($unit*3);
+				$holiday->fill(0,0,0);
+				$holiday->render();
+				break;
+			case "|":
+				echo "|";
+				usleep($unit*3);
+				break;
+			case " ";
+				echo " ";
+				usleep($unit*7)
+				break;
 		}
-		else {
-				echo "beep";
-				usleep($longdelay);
-		}
-		$holiday->fill(0,0,0);
-		$holiday->render();
 	}
-	$state = ! $state;
+	// gap between words
+	usleep($unit*7);
 }
 	
 ?>
